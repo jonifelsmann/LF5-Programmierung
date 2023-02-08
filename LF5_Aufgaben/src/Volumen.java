44a66180e33ef10052f4883ba0d6cf3e39f83481
jonathanfelsmann <jonathanfelsmann@service.oszimt.de>
2023-02-08 11:54:20 +0100

    A6.4.1 volumen

---
+++ LF5_Aufgaben/src/Volumen.java
@@ -0,0 +1,17 @@
+
+public class Volumen {
+	
+	public static void volumenberechnung(double volumenberechnung) {
+		double laenge;
+		
+		volumegesamt = laenge^3;
+		
+	}
+
+	public static void main() {
+		
+		volumenberechnung;
+		System.out.println(volumegesamt);
+	}
+	
+}
