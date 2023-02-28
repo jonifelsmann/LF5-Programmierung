27834c188416dd1816e739da203c561d29ba8e1c
jonathanfelsmann <jonathanfelsmann@service.oszimt.de>
2023-02-28 14:44:40 +0100

    7.2.1

--- LF5_Aufgaben/src/arrayuebung.java
+++ LF5_Aufgaben/src/arrayuebung.java
@@ -8,6 +8,18 @@ public class arrayuebung {
 
 		int i = 4;
 		
+	//	System.out.println(zahlen[1]); 				// 6 weil 0,1,2,3,4
+		
+	//	System.out.println(zahlen[5]); 				// error
+
+	//	System.out.println(zahlen[3]);				// 7 weil 0,1,2,3,4
+
+	//	System.out.println(zahlen[i]); 				// 2 weil i=4 und er die 4. stelle nimmt
+
+	//	System.out.println(zahlen[i-2]); 			// 3 weil i=4 -2 ist 2 und der 2. Wert vom Array
+		
+		System.out.println(zahlen[zahlen[4]]);		// 3 weil zaheln [2] und dann der 2. Wert vom Array
+		
 	}
 
 }
