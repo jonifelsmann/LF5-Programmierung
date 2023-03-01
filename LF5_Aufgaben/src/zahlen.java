1df01c517187837c65425a4955a17fa2675fda01
jonathanfelsmann <jonathanfelsmann@service.oszimt.de>
2023-03-01 12:40:12 +0100

    7.4.2 häufigste zahl

---
+++ LF5_Aufgaben/src/zahlen.java
@@ -0,0 +1,18 @@
+
+public class zahlen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int[] zahlen = {3,4,2,3,5,1,2,7,2,1,2,4};
+		
+		for (int i = 0; i <zahlen.length; i++) {
+			
+		}
+		
+		System.out.println();
+		
+	}
+
+}
