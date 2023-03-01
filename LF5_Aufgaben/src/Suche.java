6eb9ac205ce4e16fb2e14e7bf9b442d637763fde
jonathanfelsmann <jonathanfelsmann@service.oszimt.de>
2023-03-01 11:17:42 +0100

    7.4.1. suche

--- LF5_Aufgaben/src/Suche.java
+++ LF5_Aufgaben/src/Suche.java
@@ -4,8 +4,25 @@ public class Suche {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int[] zahlenArray = { 1, 5, 5, 7, 3, 11, 22, 23, 27 };
 		
+		int zahl = 5;
+		boolean gefunden = false;
 		
+		for (int i = 0; i < zahlenArray.length; i++) {
+			if (zahlenArray[i] == zahl) {
+				gefunden = true;
+				break;
+			}
+			
+		}
+		
+		if(gefunden) {
+			System.out.println("Die Zahl " + gefunden + " ist in dem Array vorhanden");
+		}
+		else {
+			System.out.println("Die Zahl " + gefunden + " ist nicht in dem Array vorhanden");
+		}
 	}
 
 }
